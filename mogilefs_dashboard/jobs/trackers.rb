0c397991627fcc687195b7752edef0a82abe8bd3
John Bertrand <john.bertrand@gmail.com>
2015-05-05 15:53:45 -0700

    Update trackers.rb

--- mogilefs_dashboard/jobs/trackers.rb
+++ mogilefs_dashboard/jobs/trackers.rb
@@ -1,9 +1,10 @@
 
 begin
 trackers = Array.new
-trackers << Tracker.new('fs01.rwc.plos.org',7001,'fs1')
-trackers << Tracker.new('fs02.rwc.plos.org',7001,'fs2')
-trackers << Tracker.new('fs03.rwc.plos.org',7001,'fs3')
+#add your trackers here
+trackers << Tracker.new('fs01.somedomain.com',7001,'fs1')
+trackers << Tracker.new('fs02.somedomain.com',7001,'fs2')
+trackers << Tracker.new('fs03.somedomain.com',7001,'fs3')
 
 interval ='1s'
 
@@ -19,6 +20,7 @@ SCHEDULER.every interval, :first_in => 0 do |job|
   trackers.each { |t| t.refresh
                       send_event(t.common_name ,  { points: t.get_points, queriesnow: t.get_queries, uptime: Time.at(t.get_uptime.to_i).utc.strftime("%H:%M:%S")}) }
 
+  #
   send_event('mogile',  {  fs1: trackers[0].get_points, fs1meta: "FS1: " + trackers[0].get_queries,fs1qps: "FS1: " + trackers[0].last_y.to_s,
                            fs2: trackers[1].get_points, fs2meta: "FS2: " + trackers[1].get_queries,fs2qps: "FS2: " + trackers[1].last_y.to_s,
                            fs3: trackers[2].get_points, fs3meta: "FS3: " + trackers[2].get_queries,fs3qps: "FS3: " + trackers[2].last_y.to_s
